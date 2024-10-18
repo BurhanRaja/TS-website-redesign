c9133e5a7a44b8639bcf43dc0965d62cf3ebf973
BurhanRaja <mails4burhan@gmail.com>
2024-10-18 21:43:07 +0530

    added ham-menu animation

---
+++ js/simple-transitions.js
@@ -0,0 +1,9 @@
+const heroTitleTextOne = document.querySelector(".hero_title_text_one");
+
+heroTitleTextOne.style.opacity="1";
+heroTitleTextOne.style.marginLeft="0px";
+
+const heroTitleTextTwo = document.querySelector(".hero_title_text_two");
+
+heroTitleTextTwo.style.opacity="1";
+heroTitleTextTwo.style.marginRight="0px";
