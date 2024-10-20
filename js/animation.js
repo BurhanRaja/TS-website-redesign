867942855ec070a5d72864fefaa5af6a50f1597a
BurhanRaja <burhanraja02@yahoo.com>
2024-10-20 19:22:14 +0530

    added animation

--- js/animation.js
+++ js/animation.js
@@ -12,17 +12,29 @@ const hamburgerNavItem_5 = document.querySelector(".hamburger_nav_item_5");
 // About
 const hamburgerNavAbout = document.querySelector(".hamburger_nav_about");
 const hamburgerNavAboutBtn = document.querySelector(".hamburger_nav_about_btn");
-const hamburgerNavAboutBtnBack = document.querySelector(".hamburger_nav_about_btn_back");
+const hamburgerNavAboutBtnBack = document.querySelector(
+  ".hamburger_nav_about_btn_back"
+);
 
 // Services
 const hamburgerNavServices = document.querySelector(".hamburger_nav_services");
-const hamburgerNavServicesBtn = document.querySelector(".hamburger_nav_services_btn");
-const hamburgerNavServicesBtnBack = document.querySelector(".hamburger_nav_services_btn_back");
+const hamburgerNavServicesBtn = document.querySelector(
+  ".hamburger_nav_services_btn"
+);
+const hamburgerNavServicesBtnBack = document.querySelector(
+  ".hamburger_nav_services_btn_back"
+);
 
 // Solutions
-const hamburgerNavSolutions = document.querySelector(".hamburger_nav_solutions");
-const hamburgerNavSolutionsBtn = document.querySelector(".hamburger_nav_solutions_btn");
-const hamburgerNavSolutionsBtnBack = document.querySelector(".hamburger_nav_solutions_btn_back");
+const hamburgerNavSolutions = document.querySelector(
+  ".hamburger_nav_solutions"
+);
+const hamburgerNavSolutionsBtn = document.querySelector(
+  ".hamburger_nav_solutions_btn"
+);
+const hamburgerNavSolutionsBtnBack = document.querySelector(
+  ".hamburger_nav_solutions_btn_back"
+);
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("is-opened");
@@ -112,7 +124,7 @@ hamburgerNavAboutBtn.addEventListener("click", (e) => {
 hamburgerNavAboutBtnBack.addEventListener("click", (e) => {
   hamburgerNavAbout.classList.remove("is_open_about");
   hamburgerNavAbout.style.transform = "translateX(1000px)";
-})
+});
 
 // Services
 hamburgerNavServicesBtn.addEventListener("click", (e) => {
@@ -128,7 +140,7 @@ hamburgerNavServicesBtn.addEventListener("click", (e) => {
 hamburgerNavServicesBtnBack.addEventListener("click", (e) => {
   hamburgerNavServices.classList.remove("is_open_services");
   hamburgerNavServices.style.transform = "translateX(1000px)";
-})
+});
 
 // Solutions
 hamburgerNavSolutionsBtn.addEventListener("click", (e) => {
@@ -144,6 +156,184 @@ hamburgerNavSolutionsBtn.addEventListener("click", (e) => {
 hamburgerNavSolutionsBtnBack.addEventListener("click", (e) => {
   hamburgerNavSolutions.classList.remove("is_open_solutions");
   hamburgerNavSolutions.style.transform = "translateX(1000px)";
-})
+});
 
 // -----------------------------------  Hamburger Navigation - End  -----------------------------------------------
+
+// --------------------------------- Know More Icon Animation - Start -------------------------------------------
+const arrowKnowMoreOnePathOne = document.querySelector(
+  "#arrow_know_more_one_path_one"
+);
+const arrowKnowMoreOnePathOneLength = arrowKnowMoreOnePathOne.getTotalLength();
+
+// Apply the calculated path length for smoother animation
+arrowKnowMoreOnePathOne.style.strokeDasharray = arrowKnowMoreOnePathOneLength;
+arrowKnowMoreOnePathOne.style.strokeDashoffset = arrowKnowMoreOnePathOneLength;
+
+const arrowKnowMoreOneText = document.querySelector(".know_more_one_text");
+arrowKnowMoreOneText.style.opacity = "1";
+arrowKnowMoreOneText.style.marginRight = "0px";
+
+// --------------------------------- Know More Icon Animation - End -------------------------------------------
+
+// --------------------------------- Social Media Hero SVG Animation - Start ----------------------------------
+// Rect One - Line One
+const socialMediaSvgHeroLineOne = document.querySelector(
+  "#social_media_svg_hero_rect_one_line_one"
+);
+const socialMediaSvgHeroLineOneLength =
+  socialMediaSvgHeroLineOne.getTotalLength();
+socialMediaSvgHeroLineOne.style.strokeDasharray =
+  socialMediaSvgHeroLineOneLength;
+socialMediaSvgHeroLineOne.style.strokeDashoffset =
+  socialMediaSvgHeroLineOneLength;
+
+// Rect One - Line Two
+const socialMediaSvgHeroLineTwo = document.querySelector(
+  "#social_media_svg_hero_rect_one_line_two"
+);
+const socialMediaSvgHeroLineTwoLength =
+  socialMediaSvgHeroLineTwo.getTotalLength();
+socialMediaSvgHeroLineTwo.style.strokeDasharray =
+  socialMediaSvgHeroLineTwoLength;
+socialMediaSvgHeroLineTwo.style.strokeDashoffset =
+  socialMediaSvgHeroLineTwoLength;
+
+// Graph Line with Arrow
+const socialMediaSvgHeroGraphLineOne = document.querySelector(
+  "#social_media_svg_hero_graph_line_one"
+);
+const socialMediaSvgHeroGraphLineTwo = document.querySelector(
+  "#social_media_svg_hero_graph_line_two"
+);
+const socialMediaSvgHeroGraphLineThree = document.querySelector(
+  "#social_media_svg_hero_graph_line_three"
+);
+const socialMediaSvgHeroGraphLineFour = document.querySelector(
+  "#social_media_svg_hero_graph_line_four"
+);
+const socialMediaSvgHeroGraphLineFive = document.querySelector(
+  "#social_media_svg_hero_graph_line_five"
+);
+const socialMediaSvgHeroGraphLineSix = document.querySelector(
+  "#social_media_svg_hero_graph_line_six"
+);
+const socialMediaSvgHeroGraphLineSeven = document.querySelector(
+  "#social_media_svg_hero_graph_line_seven"
+);
+
+const socialMediaSvgHeroGraphLineOneLength =
+  socialMediaSvgHeroGraphLineOne.getTotalLength();
+socialMediaSvgHeroGraphLineOne.style.strokeDasharray =
+  socialMediaSvgHeroGraphLineOneLength;
+socialMediaSvgHeroGraphLineOne.style.strokeDashoffset =
+  socialMediaSvgHeroGraphLineOneLength;
+
+const socialMediaSvgHeroGraphLineTwoLength =
+  socialMediaSvgHeroGraphLineTwo.getTotalLength();
+socialMediaSvgHeroGraphLineTwo.style.strokeDasharray =
+  socialMediaSvgHeroGraphLineTwoLength;
+socialMediaSvgHeroGraphLineTwo.style.strokeDashoffset =
+  socialMediaSvgHeroGraphLineTwoLength;
+
+const socialMediaSvgHeroGraphLineThreeLength =
+  socialMediaSvgHeroGraphLineThree.getTotalLength();
+socialMediaSvgHeroGraphLineThree.style.strokeDasharray =
+  socialMediaSvgHeroGraphLineThreeLength;
+socialMediaSvgHeroGraphLineThree.style.strokeDashoffset =
+  socialMediaSvgHeroGraphLineThreeLength;
+
+const socialMediaSvgHeroGraphLineFourLength =
+  socialMediaSvgHeroGraphLineFour.getTotalLength();
+socialMediaSvgHeroGraphLineFour.style.strokeDasharray =
+  socialMediaSvgHeroGraphLineFourLength;
+socialMediaSvgHeroGraphLineFour.style.strokeDashoffset =
+  socialMediaSvgHeroGraphLineFourLength;
+
+const socialMediaSvgHeroGraphLineFiveLength =
+  socialMediaSvgHeroGraphLineFive.getTotalLength();
+socialMediaSvgHeroGraphLineFive.style.strokeDasharray =
+  socialMediaSvgHeroGraphLineFiveLength;
+socialMediaSvgHeroGraphLineFive.style.strokeDashoffset =
+  socialMediaSvgHeroGraphLineFiveLength;
+
+const socialMediaSvgHeroGraphLineSixLength =
+  socialMediaSvgHeroGraphLineSix.getTotalLength();
+socialMediaSvgHeroGraphLineSix.style.strokeDasharray =
+  socialMediaSvgHeroGraphLineSixLength;
+socialMediaSvgHeroGraphLineSix.style.strokeDashoffset =
+  socialMediaSvgHeroGraphLineSixLength;
+
+const socialMediaSvgHeroGraphLineSevenLength =
+  socialMediaSvgHeroGraphLineSeven.getTotalLength();
+socialMediaSvgHeroGraphLineSeven.style.strokeDasharray =
+  socialMediaSvgHeroGraphLineSevenLength;
+socialMediaSvgHeroGraphLineSeven.style.strokeDashoffset =
+  socialMediaSvgHeroGraphLineSevenLength;
+
+// Instagram Gray
+const socialMediaSvgHeroInstagramGray = document.querySelector(
+  "#social_media_svg_hero_instagram_gray"
+);
+const socialMediaSvgHeroInstagramGrayLength =
+  socialMediaSvgHeroInstagramGray.getTotalLength();
+socialMediaSvgHeroInstagramGray.style.strokeDasharray =
+  socialMediaSvgHeroInstagramGrayLength;
+socialMediaSvgHeroInstagramGray.style.strokeDashoffset =
+  socialMediaSvgHeroInstagramGrayLength;
+
+// Instagram Green
+const socialMediaSvgHeroInstagramGreen = document.querySelector(
+  "#social_media_svg_hero_instagram_green"
+);
+const socialMediaSvgHeroInstagramGreenLength =
+  socialMediaSvgHeroInstagramGreen.getTotalLength();
+socialMediaSvgHeroInstagramGreen.style.strokeDasharray =
+  socialMediaSvgHeroInstagramGreenLength;
+socialMediaSvgHeroInstagramGreen.style.strokeDashoffset =
+  socialMediaSvgHeroInstagramGreenLength;
+
+// Linkedin Gray
+const socialMediaSvgHeroLinkedInGray = document.querySelector(
+  "#social_media_svg_hero_linkedin_gray"
+);
+const socialMediaSvgHeroLinkedInGrayLength =
+  socialMediaSvgHeroLinkedInGray.getTotalLength();
+socialMediaSvgHeroLinkedInGray.style.strokeDasharray =
+  socialMediaSvgHeroLinkedInGrayLength;
+socialMediaSvgHeroLinkedInGray.style.strokeDashoffset =
+  socialMediaSvgHeroLinkedInGrayLength;
+
+// Linkedin Green
+const socialMediaSvgHeroLinkedInGreen = document.querySelector(
+  "#social_media_svg_hero_linkedin_green"
+);
+const socialMediaSvgHeroLinkedInGreenLength =
+  socialMediaSvgHeroLinkedInGreen.getTotalLength();
+socialMediaSvgHeroLinkedInGreen.style.strokeDasharray =
+  socialMediaSvgHeroLinkedInGreenLength;
+socialMediaSvgHeroLinkedInGreen.style.strokeDashoffset =
+  socialMediaSvgHeroLinkedInGreenLength;
+
+// Youtube Gray
+const socialMediaSvgHeroYoutubeGray = document.querySelector(
+  "#social_media_svg_hero_youtube_gray"
+);
+const socialMediaSvgHeroYoutubeGrayLength =
+  socialMediaSvgHeroYoutubeGray.getTotalLength();
+socialMediaSvgHeroYoutubeGray.style.strokeDasharray =
+  socialMediaSvgHeroYoutubeGrayLength;
+socialMediaSvgHeroYoutubeGray.style.strokeDashoffset =
+  socialMediaSvgHeroYoutubeGrayLength;
+
+// Youtube Green
+const socialMediaSvgHeroYoutubeGreen = document.querySelector(
+  "#social_media_svg_hero_youtube_green"
+);
+const socialMediaSvgHeroYoutubeGreenLength =
+  socialMediaSvgHeroYoutubeGreen.getTotalLength();
+socialMediaSvgHeroYoutubeGreen.style.strokeDasharray =
+  socialMediaSvgHeroYoutubeGreenLength;
+socialMediaSvgHeroYoutubeGreen.style.strokeDashoffset =
+  socialMediaSvgHeroYoutubeGreenLength;
+// --------------------------------- Social Media Hero SVG Animation - End ----------------------------------
--- js/simple-transitions.js
+++ js/simple-transitions.js
@@ -1,9 +1,103 @@
-const heroTitleTextOne = document.querySelector(".hero_title_text_one");
+function inView(element) {
+  // get window height
+  var windowHeight = window.innerHeight;
+  // get number of pixels that the document is scrolled
+  var scrollY = window.scrollY || window.pageYOffset;
 
-heroTitleTextOne.style.opacity="1";
-heroTitleTextOne.style.marginLeft="0px";
+  // get current scroll position (distance from the top of the page to the bottom of the current viewport)
+  var scrollPosition = scrollY + windowHeight;
+  // get element position (distance from the top of the page to the bottom of the element)
+  var elementPosition =
+    element.getBoundingClientRect().top + scrollY + elementHeight;
+
+  // is scroll position greater than element position? (is element in view?)
+  if (scrollPosition > elementPosition) {
+    return true;
+  }
+
+  return false;
+}
+
+// -------------------- Hero Section Title - Start ------------------
+const heroTitleTextOne = document.querySelector(".hero_title_text_one");
+heroTitleTextOne.style.opacity = "1";
+heroTitleTextOne.style.marginLeft = "0px";
 
 const heroTitleTextTwo = document.querySelector(".hero_title_text_two");
+heroTitleTextTwo.style.opacity = "1";
+heroTitleTextTwo.style.marginRight = "0px";
 
-heroTitleTextTwo.style.opacity="1";
-heroTitleTextTwo.style.marginRight="0px";
+const text =
+  "We are a web design agency helping businesses of all sizes get a better return from online activities. We design and build stunning bespoke websites. And we can help with a wide range of online marketing strategies. We have a complete Forex CRM Solution under one roof with Techysquad - The best Forex CRM Agency.";
+
+let index = 0;
+const speed = 10; // typing speed
+
+function typeWriter() {
+  if (index < text.length) {
+    document.getElementById("hero_description_text").innerHTML +=
+      text.charAt(index);
+    index++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
+window.onload = function () {
+  typeWriter();
+};
+// -------------------- Hero Section Title - End-----------------
+
+// -------------------- Social Media Hero SVG - Start ------------------
+// Rectangle - 1
+const socialMediaSvgHeroRectOne = document.querySelector(
+  ".social_media_svg_hero_rect_one"
+);
+socialMediaSvgHeroRectOne.style.opacity = "1";
+socialMediaSvgHeroRectOne.style.transform = "translateX(0px)";
+
+// Rectangle - 2
+const socialMediaSvgHeroRectTwo = document.querySelector(
+  ".social_media_svg_hero_rect_two"
+);
+socialMediaSvgHeroRectTwo.style.opacity = "1";
+socialMediaSvgHeroRectTwo.style.transform = "translateX(0px)";
+
+// Icons
+const socialMediaSvgHeroOneInstagram = document.querySelector(
+  "#social_media_svg_hero_one_instagram"
+);
+const socialMediaSvgHeroOneLinkedin = document.querySelector(
+  "#social_media_svg_hero_one_linkedin"
+);
+const socialMediaSvgHeroOneYoutube = document.querySelector(
+  "#social_media_svg_hero_one_youtube"
+);
+socialMediaSvgHeroOneInstagram.style.opacity = "1";
+socialMediaSvgHeroOneLinkedin.style.opacity = "1";
+socialMediaSvgHeroOneYoutube.style.opacity = "1";
+
+// Text
+const socialMediaSvgHeroInstagramText = document.querySelector(
+  "#social_media_svg_hero_instagram_text"
+);
+const socialMediaSvgHeroLinkedinText = document.querySelector(
+  "#social_media_svg_hero_linkedin_text"
+);
+const socialMediaSvgHeroYoutubeText = document.querySelector(
+  "#social_media_svg_hero_youtube_text"
+);
+socialMediaSvgHeroInstagramText.style.opacity = "1";
+socialMediaSvgHeroInstagramText.style.transform = "translateX(0px)";
+socialMediaSvgHeroLinkedinText.style.opacity = "1";
+socialMediaSvgHeroLinkedinText.style.transform = "translateX(0px)";
+socialMediaSvgHeroYoutubeText.style.opacity = "1";
+socialMediaSvgHeroYoutubeText.style.transform = "translateX(0px)";
+
+// Text Two
+const socilaMediaSvgHeroRectTwoText = document.querySelector(
+  "#social_media_svg_hero_rect_two_text"
+);
+socilaMediaSvgHeroRectTwoText.style.opacity = "1";
+socilaMediaSvgHeroRectTwoText.style.transform = "translateX(0px)";
+
+// -------------------- Social Media Hero SVG - End ------------------
